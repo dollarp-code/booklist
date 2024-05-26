7acb8bfb9f09eb248235591fcb434fb6b7bdcb89
dollarp-code <husseindollarp@yahoo.com>
2024-05-27 00:11:22 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Booklist() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81MYaU742TL._AC_UL600_SR600,400_.jpg"
+    alt="The End of Everything: How Wars Descend into Annihilation"
+  />
+);
+const Title = () => (
+  <h2>The End of Everything: How Wars Descend into Annihilation</h2>
+);
+const Author = () => {
+  return <h4>Victor Davis Hanson</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Booklist />);
