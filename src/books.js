47c550a453cf77fab4d48d14855891f2a288978c
dollarp-code <husseindollarp@yahoo.com>
2024-05-27 21:19:03 +0100

    Understanding Es6 modules

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: 'Victor Davis Hanson',
+    title: 'The End of Everything: How Wars Descend into Annihilation',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81MYaU742TL._AC_UL600_SR600,400_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Jeffrey Mason',
+    title: 'Dad, I Want to Hear Your Story',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51ZvZFJOsrL._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,33 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+import { books } from './books';
+import Book from './Book';
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
-
-const author = 'Victor Davis Hanson';
-const Book = () => {
-  const title = 'The End of Everything: How Wars Descend into Annihilation';
-  return (
-    <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81MYaU742TL._AC_UL600_SR600,400_.jpg"
-        alt="The End of Everything: How Wars Descend into Annihilation"
-      />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let p}</p> */}
-      <p>{6 + 6}</p>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
