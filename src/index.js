bf533554cc7df521593c284da8d98bdd54f931ef
dollarp-code <husseindollarp@yahoo.com>
2024-05-27 01:58:29 +0100

    Understanding JSX css and JSX javascript

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function Booklist() {
   return (
-    <section>
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
       <Book />
     </section>
   );
 }
 
+const author = 'Victor Davis Hanson';
 const Book = () => {
+  const title = 'The End of Everything: How Wars Descend into Annihilation';
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81MYaU742TL._AC_UL600_SR600,400_.jpg"
+        alt="The End of Everything: How Wars Descend into Annihilation"
+      />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      {/* <p>{let p}</p> */}
+      <p>{6 + 6}</p>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81MYaU742TL._AC_UL600_SR600,400_.jpg"
-    alt="The End of Everything: How Wars Descend into Annihilation"
-  />
-);
-const Title = () => (
-  <h2>The End of Everything: How Wars Descend into Annihilation</h2>
-);
-const Author = () => {
-  return <h4>Victor Davis Hanson</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
